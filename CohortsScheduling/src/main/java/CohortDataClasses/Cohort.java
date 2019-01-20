91a42896fba651bd8671cc15e656ed8d510d83fc
Grant Farnsworth <grantfar@gmail.com>
2019-01-20 16:49:34 -0500

    added alexes work on data classes

--- CohortsScheduling/src/main/java/CohortDataClasses/Cohort.java
+++ CohortsScheduling/src/main/java/CohortDataClasses/Cohort.java
@@ -1,6 +1,39 @@
-package CohortDataClasses;
+package main.java.CohortDataClasses;
+
+import java.util.List;
+
 public class Cohort {
-    private Course[] requirements;
-    private int seats;
+	
+	private List<Course> requirements;
+    private List<Section> enrolledClasses;
+    private String name;
+
+	public Cohort() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Course> getRequirements() {
+		return requirements;
+	}
+
+	public void setRequirements(List<Course> requirements) {
+		this.requirements = requirements;
+	}
+
+	public List<Section> getEnrolledClasses() {
+		return enrolledClasses;
+	}
+
+	public void setEnrolledClasses(List<Section> enrolledClasses) {
+		this.enrolledClasses = enrolledClasses;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
