fbbb5a57ba8834310acdb120804296d826dd5e2f
Grant Farnsworth <grantfar@gmail.com>
2019-01-21 19:22:28 -0500

    Finished SolverConfig Code

--- CohortsScheduling/src/main/java/Main.java
+++ CohortsScheduling/src/main/java/Main.java
@@ -1,14 +1,18 @@
 import org.optaplanner.core.api.solver.*;
-import java.io.File;
 import CohortsSolverData.CohortSolution;
 public class Main {
     public static void main()
     {
+    	CohortSolution solutions[] = new CohortSolution[20];
+    	SolverFactory<CohortSolution> factory = SolverFactory.createFromXmlResource("SolverConfig.xml");
+    	Solver<CohortSolution> solver = factory.buildSolver();
     	
-    	Solver solver = SolverFactory.createFromXmlResource("SolverConfig.xml").buildSolver();
+    	for(int i = 0; i<20 ;i++){
+    		CohortSolution BestSolution = new CohortSolution();
+        	BestSolution = (CohortSolution)solver.solve(BestSolution);
+        	solutions[i] = BestSolution;
+    	}
     	
-    	CohortSolution BestSolution = new CohortSolution();
     	
-    	BestSolution = (CohortSolution)solver.solve(BestSolution);
     }
 }
