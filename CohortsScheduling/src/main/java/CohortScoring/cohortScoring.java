9d7decd6512d135094fea617ca0f5c75013e564c
Grant Farnsworth <grantfar@gmail.com>
2019-01-23 18:55:14 -0500

    started to fix scoring

--- CohortsScheduling/src/main/java/CohortScoring/cohortScoring.java
+++ CohortsScheduling/src/main/java/CohortScoring/cohortScoring.java
@@ -3,11 +3,11 @@ package CohortScoring;
 import CohortDataClasses.*;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.impl.score.director.easy.EasyScoreCalculator;
-
+import CohortsSolverData.CohortSolution;
 public class cohortScoring implements EasyScoreCalculator {
 
 
-    public HardSoftScore calculateScore(Object o) {
+    public HardSoftScore calculateScore(CohortSolution o) {
         return HardSoftScore.valueOf(
                 HardScoringFunctions.scoreSchedule((Cohort)o),
                 SoftScoringFunctions.scoreSchedule((Cohort)o)
