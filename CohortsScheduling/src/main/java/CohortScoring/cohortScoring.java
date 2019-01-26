b009e768538398813b4261c0fa5134984964add3
Grant Farnsworth <grantfar@gmail.com>
2019-01-26 13:43:53 -0500

    worked on addapting scorring functions. Added junit

--- CohortsScheduling/src/main/java/CohortScoring/cohortScoring.java
+++ CohortsScheduling/src/main/java/CohortScoring/cohortScoring.java
@@ -7,10 +7,10 @@ import CohortsSolverData.CohortSolution;
 public class cohortScoring implements EasyScoreCalculator {
 
 
-    public HardSoftScore calculateScore(CohortSolution o) {
+    public HardSoftScore calculateScore(Object o) {
         return HardSoftScore.valueOf(
-                HardScoringFunctions.scoreSchedule((Cohort)o),
-                SoftScoringFunctions.scoreSchedule((Cohort)o)
+                HardScoringFunctions.scoreSchedule((CohortSolution)o),
+                SoftScoringFunctions.scoreSchedule((CohortSolution)o)
         );
     }
 }
