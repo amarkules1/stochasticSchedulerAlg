abd8c820ef3efe2bc1c1ea4395df3730cee85f18
JakeKampf <jacob.kampf@gmail.com>
2019-01-26 16:35:38 -0500

    Added function calls of file reader to main, still have to test

--- CohortsScheduling/src/main/java/Main.java
+++ CohortsScheduling/src/main/java/Main.java
@@ -1,8 +1,21 @@
 import org.optaplanner.core.api.solver.*;
-import CohortsSolverData.CohortSolution;
+import CohortsSolverData.CohortSolution; 
+import CohortDataClasses.*;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
-    public static void main()
+	
+    public static void main() throws FileNotFoundException
     {
+    	
+    	FileReader fileReader = new FileReader();
+    	List<Course> courseList = new ArrayList<Course>(); 
+    	List<Cohort> cohortList = new ArrayList<Cohort>();
+    	
+    	courseList = fileReader.readClassFile("fileName", courseList); 
+    	cohortList = fileReader.readCohortFile("fileName", cohortList);
+    	
     	CohortSolution solutions[] = new CohortSolution[20];
     	SolverFactory<CohortSolution> factory = SolverFactory.createFromXmlResource("SolverConfig.xml");
     	Solver<CohortSolution> solver = factory.buildSolver();
