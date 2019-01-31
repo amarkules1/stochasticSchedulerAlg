3dc3e0829eb2d5feaef599e2d87ee24d3c409c45
JakeKampf <jacob.kampf@gmail.com>
2019-01-31 17:01:55 -0500

    add Date initialization in file reader

--- CohortsScheduling/src/main/java/CohortDataClasses/FileReader.java
+++ CohortsScheduling/src/main/java/CohortDataClasses/FileReader.java
@@ -1,7 +1,8 @@
 package CohortDataClasses;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,17 +14,19 @@ public class FileReader {
 		 
 	}
 	
-	public List<Course> readClassFile(String fileName, List<Course> courseList) throws FileNotFoundException{
+	public List<Course> readClassFile(String fileName, List<Course> courseList) throws FileNotFoundException, ParseException{
 		File file = new File(fileName);
 		Scanner scan = new Scanner(file); 
 		String line;
 		String[] tempTime = new String[2];
 		String[] field = new String[14];
+		String tempDay = "01/01/2019";
 		Course course = new Course();
 		List<Section> sections = new ArrayList<Section>();
-		Section section = new Section();
+		Section section = new Section(); 
 		
-		scan.nextLine();	//skip title line
+		//skip title line
+		scan.nextLine();	
 		line = scan.nextLine();
 		field = line.split(",");
 		course.setName(field[4]); 
@@ -33,7 +36,8 @@ public class FileReader {
 		section.setName(field[4]);
 		section.setLink(field[5]); 
 		section.setDaysOfWeek(field[8]);
-		tempTime = field[9].split(" - "); 
+		tempTime = field[9].split(" - ");  
+		
 		//start time
 		//end time
 		section.setBuilding(field[10]);
@@ -50,15 +54,17 @@ public class FileReader {
 			
 			//check to see if same course, if so add to sections list
 			if(course.getName().compareTo(field[0])==0) { 
-				section.setSectionId(field[1]);
+				section.setSectionId(field[3]);
 				section.setCrn(Integer.parseInt(field[2]));
-				section.setSection(Integer.parseInt(field[3])); 
 				section.setName(field[4]);
 				section.setLink(field[5]); 
 				section.setDaysOfWeek(field[8]); 
 				tempTime = field[9].split(" - "); 
 				//start time
+				SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy HH:mm"); 
+				section.setStartTime(format.parse(tempDay+ " " + tempTime[0]));
 				//end time
+				section.setEndTime(format.parse(tempDay+ " "+ tempTime[1]));
 				section.setBuilding(field[10]);
 				section.setRoom(field[11]);
 				section.setInstructor(field[12]);
@@ -72,14 +78,16 @@ public class FileReader {
 				course.setSections(sections);
 				courseList.add(course);
 				course.setName((field[4]));
-				section.setSectionId(field[1]);
+				section.setSectionId(field[3]);
 				section.setCrn(Integer.parseInt(field[2]));
-				section.setSection(Integer.parseInt(field[3])); 
 				section.setName(field[4]);
 				section.setLink(field[5]); 
 				tempTime = field[9].split(" - "); 
 				//start time
+				SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy HH:mm"); 
+				section.setStartTime(format.parse(tempDay+ " " + tempTime[0]));
 				//end time
+				section.setEndTime(format.parse(tempDay+ " "+ tempTime[1]));
 				section.setDaysOfWeek(field[8]); 
 				section.setBuilding(field[10]);
 				section.setRoom(field[11]);
@@ -103,7 +111,8 @@ public class FileReader {
 		Course requirement = new Course();
 		int index=0;
 		
-		scan.nextLine();		//skip title line
+		//skip title line
+		scan.nextLine();		
 		
 		line = scan.nextLine();
 		field = line.split(",");
