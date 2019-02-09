426897897710109e1242237d8d2d1ceefbb9f416
Alex Markules <amarlar2354@gmail.com>
2019-02-09 17:52:18 -0500

    set up some data classes

---
+++ CohortsScheduling/src/main/java/service/RestService.java
@@ -0,0 +1,20 @@
+package service;
+
+public class RestService {
+
+	public static String start() {
+		return null;
+		//starts scheduling
+	}
+	
+	public static String status() {
+		return null;
+		
+	}
+	
+	public static String cancel() {
+		return null;
+		
+	}
+
+}
